f32343a48a8dcc7206e609deb9cb639fa7385af4
zoan37 <104385984+zoan37@users.noreply.github.com>
2024-06-09 14:14:33 -0400

    support prod blob store

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,16 @@ const nextConfig = {
                 hostname: 'fal.media',
                 pathname: '**'
             },
+            // test blob store
             {
                 protocol: 'https',
                 hostname: 'ldu9dipdwsmvpeca.public.blob.vercel-storage.com',
+                pathname: '**'
+            },
+            // prod blob store
+            {
+                protocol: 'https',
+                hostname: 'dh2jt8inkxocsggz.public.blob.vercel-storage.com',
                 pathname: '**'
             }
         ]
